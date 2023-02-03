d9e42eebca158e325f6fa99a219d3514cd9cf7c3
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-02-03 09:34:53 -0500

    import react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import * as React from 'react';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
