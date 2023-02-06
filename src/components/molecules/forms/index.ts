1665de24d6fa69b8ce9607e478eab4d14fe5c2b1
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-02-06 01:54:02 -0500

    Delete index.ts

--- src/components/molecules/forms/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as AccountSecurityUpdateForm } from './account-security-update.molecule';
-export { default as AccountUpdateForm } from './account-update.molecule';
-export { default as LoginForm } from './login.molecule';
-export { default as PasswordForgotForm } from './password-forgot.molecule';
-export { default as PasswordResetForm } from './password-reset.molecule';
-export { default as RegisterForm } from './register.molecule';
