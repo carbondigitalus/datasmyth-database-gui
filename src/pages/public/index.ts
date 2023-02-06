dca65abea5e41517b61c58ad7ec482f23cd9df92
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-02-06 00:07:27 -0500

    delete public pages

--- src/pages/public/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LoginPage } from './login.page';
-export { default as PasswordForgotPage } from './password-forgot.page';
-export { default as PasswordResetPage } from './password-reset.page';
-export { default as RegisterPage } from './register.page';
