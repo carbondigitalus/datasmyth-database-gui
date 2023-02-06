c3502cc5801870c1d73198995d41318738f5c40d
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-02-06 01:38:36 -0500

    update color

--- src/pages/portal/portal.page.tsx
+++ src/pages/portal/portal.page.tsx
@@ -11,7 +11,7 @@ export default class PortalPage extends React.Component {
   render() {
     return (
       <PortalTemplate pageTitle='Dashboard'>
-        <Box component='main' sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
+        <Box component='main' sx={{ flexGrow: 1, bgcolor: 'white', p: 3 }}>
           <Toolbar />
           <Typography paragraph>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
