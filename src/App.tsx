71c8bfbb7f669aac22985e4964285b403da81ccb
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-02-06 01:37:15 -0500

    delete unused pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,13 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // Custom Modules
-import { AccountPage, PortalPage } from 'pages/portal';
-import { LoginPage, PasswordForgotPage, PasswordResetPage, RegisterPage } from 'pages/public';
+import { PortalPage } from 'pages/portal';
 
 export default class App extends React.Component {
   public router = createBrowserRouter([
     {
       path: '/',
       element: <PortalPage />
-    },
-    {
-      path: '/account',
-      element: <AccountPage />
-    },
-    {
-      path: '/account/settings',
-      element: <PortalPage />
-    },
-    {
-      path: '/password-forgot',
-      element: <PasswordForgotPage />
-    },
-    {
-      path: '/login',
-      element: <LoginPage />
-    },
-    {
-      path: '/register',
-      element: <RegisterPage />
-    },
-    {
-      path: '/password-reset',
-      element: <PasswordResetPage />
     }
   ]);
   render() {
