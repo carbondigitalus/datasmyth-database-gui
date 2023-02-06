77d923c02beb5d4d31cd354508576d0c10cce1d9
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-02-06 01:38:06 -0500

    setup account page as clone of portal with context

--- src/pages/portal/account.page.tsx
+++ src/pages/portal/account.page.tsx
@@ -1,17 +1,13 @@
 // NPM Modules
 import * as React from 'react';
-import PersonIcon from '@mui/icons-material/Person';
-import SecurityIcon from '@mui/icons-material/Security';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import { Avatar, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper } from '@mui/material';
+import { Typography } from '@mui/material';
 import $ from 'jquery';
 
 // Custom Modules
 import { PortalTemplate } from '../../components/templates';
 import AccountContext from 'utils/context/account.context';
-import { AccountUpdateWrapper } from 'components/organisms';
-import { AccountSecurityUpdateForm, AccountUpdateForm } from 'components/molecules/forms';
 
 export default class AccountPage extends React.Component {
   static contextType = AccountContext;
@@ -22,81 +18,25 @@ export default class AccountPage extends React.Component {
     // this.onFirstLoadHideSections();
   }
 
-  componentDidMount(): void {
-    this.onFirstLoadHideSections();
-  }
-
-  private onFirstLoadHideSections() {
-    if (!window.location.href.includes('#')) {
-      console.log('false');
-      $('#Security-update-subpage').hide();
-    }
-  }
-
-  private onClickDisplay(event) {
-    const subPageToDisplay = event.target.innerText;
-    console.log('subpagetodisplay', subPageToDisplay);
-    $('#Account-update-subpage').hide();
-    $('#Security-update-subpage').removeClass('display-none');
-    $('#Security-update-subpage').hide();
-    $(`#${subPageToDisplay}-update-subpage`).show();
-  }
-
   render() {
     return (
       <PortalTemplate pageTitle='Account'>
         <AccountContext.Consumer>
           {(context) => (
-            <Box component='main' sx={{ flexGrow: 1, bgcolor: 'background.default', p: 5 }}>
+            <Box component='main' sx={{ flexGrow: 1, bgcolor: 'white', p: 3 }}>
               <Toolbar />
-              <Grid container>
-                <Grid item xs={10} sx={{ marginLeft: 'auto', marginRight: 'auto' }}>
-                  <Paper elevation={3} sx={{ backgroundColor: 'white', p: 5, minHeight: '80vh', display: 'flex' }}>
-                    <Grid item xs={2}>
-                      <List>
-                        <ListItem key={'Avatar'} disablePadding>
-                          <Avatar
-                            sx={{ width: 80, height: 80, marginRight: 'auto', marginLeft: 'auto', fontSize: '40px' }}
-                          >
-                            {context?.firstName.substring(0, 1)}
-                            {context?.lastName.substring(0, 1)}
-                          </Avatar>
-                        </ListItem>
-                        <ListItem key={'Account'} disablePadding sx={{ marginTop: '25px' }}>
-                          <ListItemButton component='a' href='#account-update-subpage' onClick={this.onClickDisplay}>
-                            <ListItemIcon>
-                              <PersonIcon />
-                            </ListItemIcon>
-                            <ListItemText primary={'Account'} />
-                          </ListItemButton>
-                        </ListItem>
-                        <ListItem key={'Security'} disablePadding>
-                          <ListItemButton component='a' href='#security-update-subpage' onClick={this.onClickDisplay}>
-                            <ListItemIcon>
-                              <SecurityIcon />
-                            </ListItemIcon>
-                            <ListItemText primary='Security' />
-                          </ListItemButton>
-                        </ListItem>
-                      </List>
-                    </Grid>
-                    <Grid item xs={10}>
-                      <List>
-                        <ListItem id='Account-update-subpage'>
-                          <AccountUpdateWrapper subPageTitle='Personal Information'>
-                            <AccountUpdateForm />
-                          </AccountUpdateWrapper>
-                        </ListItem>
-                        <ListItem id='Security-update-subpage' className='display-none'>
-                          <AccountUpdateWrapper subPageTitle='Security Information'>
-                            <AccountSecurityUpdateForm />
-                          </AccountUpdateWrapper>
-                        </ListItem>
-                      </List>
-                    </Grid>
-                  </Paper>
-                </Grid>
-              </Grid>
+              <Typography paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus at
+                ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis
+                convallis tellus id interdum velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+                adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
+                viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+                quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget arcu
+                dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+                faucibus et molestie ac.
+              </Typography>
+              <Typography paragraph>{context.email}</Typography>
             </Box>
           )}
         </AccountContext.Consumer>
