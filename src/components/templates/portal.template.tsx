18412bfc46e2952bfd804c2b4cbedb8b6b168fa6
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-02-09 00:05:57 -0500

    delete unused imports

--- src/components/templates/portal.template.tsx
+++ src/components/templates/portal.template.tsx
@@ -27,7 +27,6 @@ import {
 import * as React from 'react';
 
 // Custom Modules
-import { Project } from './../../utils/smythdb-core/Project';
 import { TableOptions } from 'utils/smythdb-core/TableOptions';
 import DatabaseContext from 'utils/context/database.context';
 import database from './../../database/database.json';
