ae7493455feef2858b79ca37798e11bbd824e884
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-02-10 19:05:22 -0500

    import @mui/material components

--- src/pages/table/[tableName].tsx
+++ src/pages/table/[tableName].tsx
@@ -1,2 +1,16 @@
 // NPM Modules
 import * as React from 'react';
+import {
+  Box,
+  Chip,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Toolbar,
+  Typography
+} from '@mui/material';
