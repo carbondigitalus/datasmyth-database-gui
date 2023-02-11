02f2bb14a2d74d5ca77f31543bfee88daddab212
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-02-10 19:08:49 -0500

    setup data table

--- src/pages/table/[tableName].tsx
+++ src/pages/table/[tableName].tsx
@@ -44,6 +44,51 @@ export async function getStaticPaths() {
 }
 
 class TableNamePage extends React.Component<PageProps> {
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 700 }} aria-label='customized table'>
+                <TableHead>
+                  <TableRow sx={{ borderTop: 'solid 1px rgba(0,0,0,0.1)' }}>
+                    <TableCell sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>Name</TableCell>
+                    <TableCell sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>Type</TableCell>
+                    <TableCell sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>Settings</TableCell>
+                    <TableCell sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>References</TableCell>
+                    <TableCell sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>Default</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {database.tableList.map((item) => {
+                    // check to make sure the page url & defined table
+                    // match the table's name being checked
+                    if (item.name === this.props.router.asPath.split('/table/')[1]) {
+                      // create array of keys
+                      const tableKeys = Object.keys(item);
+                      // delete first item (name) from array
+                      tableKeys.shift();
+                      //
+                      return tableKeys.map((fieldName) => {
+                        const column: ColumnOptions = item[fieldName];
+                        return (
+                          <TableRow key={`${item}-${column}`}>
+                            <TableCell component='th' scope='row' sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>
+                              {fieldName}
+                            </TableCell>
+                            <TableCell sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>{column.type}</TableCell>
+                            <TableCell sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>
+                              <Stack direction='row'>{this.settingCheck(column)}</Stack>
+                            </TableCell>
+                            <TableCell sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>
+                              {column?.ref?.refField}
+                            </TableCell>
+                            <TableCell sx={{ borderRight: 'solid 1px rgba(0,0,0,0.1)' }}>{column.default}</TableCell>
+                          </TableRow>
+                        );
+                      });
+                    }
+                    return;
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
   private settingCheck(column: ColumnOptions) {
     let options: string[] = [];
     if (column.nullable === true) {
